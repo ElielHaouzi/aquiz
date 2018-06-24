3ec912fecd8be66cfe0d3989552afe40db64ffae
Eliel Haouzi <eliel.haouzi@gmail.com>
2018-06-24 18:34:23 +0300

    sow commands

--- lib/aquiz/commands/command.ex
+++ lib/aquiz/commands/command.ex
@@ -1,4 +1,4 @@
 defmodule Command do
   @callback display() :: {:ok, String.t}
-  @callback execute() :: {:ok, term} | {:error, String.t}
+  @callback execute() :: {:ok} | {:stop} | {:error, String.t}
 end
--- lib/aquiz/commands/end_quiz.ex
+++ lib/aquiz/commands/end_quiz.ex
@@ -2,10 +2,16 @@ defmodule AQuiz.Command.EndQuiz do
   @behaviour Command
 
   def display() do
-    {:ok, "Stop the quiz"}
+    {:ok, "Stop the quiz?"}
   end
 
-  def execute() do
-    {:error, ""}
+  def execute(input) do
+    display_result()
+    {:stop}
+  end
+
+  defp display_result() do
+    points = Aquiz.Quiz.get_points()
+    IO.puts("\nPoints: #{points}")
   end
 end
